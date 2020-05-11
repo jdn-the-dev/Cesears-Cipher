6ead0c15c0e639b93a5840a45de894a98198ae4e
jdn-the-dev <65180821+jdn-the-dev@users.noreply.github.com>
2020-05-11 11:54:33 -0400

    Update CeaserCipher.java

--- CeaserCipher.java
+++ CeaserCipher.java
@@ -2,14 +2,14 @@ public class CeaserCipher {
     public static StringBuffer cipher(String original, int shifts) {
         StringBuffer cipheredMessage = new StringBuffer();      
         for (int i = 0; i < original.length(); i++) {
-            cipheredMessage.append((char)((original.toUpperCase().charAt(i) + shifts - 65) % 26 + 65));
+            cipheredMessage.append((char)(((int)original.toUpperCase().charAt(i) + shifts - 65) % 26 + 65));
         }
         return cipheredMessage;
     }
     public static StringBuffer decrypt(String message, int shifts) {
         StringBuffer originalMessage = new StringBuffer();      
         for (int i = 0; i < message.length(); i++) {
-            originalMessage.append((char)((message.toUpperCase().charAt(i) - shifts - 65) % 26 + 65));
+            originalMessage.append((char)(((int)message.toUpperCase().charAt(i) - shifts - 65) % 26 + 65));
         }
         return originalMessage;
     }
